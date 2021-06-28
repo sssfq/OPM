d68ecfab01e0eea903ceddd03f25bc4bad65b782
shenf <404503167@qq.com>
2021-06-28 10:53:20 +0800

    create optisim_gen directory for Optisystem simulation data generation

--- optisim_gen/version_convert.m
+++ optisim_gen/version_convert.m
@@ -1,6 +1,6 @@
 clear all
 
-Rolloff = [0.01 0.03 0.05 0.08 0.1 0.2 0.3 0.4 0.5 0.6];
+OSNR = 10:2:20;
 %% Convert current .mat to version 7.3
 for i = 1:length(Rolloff)
     Path = 'F:\tempo sim data\224Gbpers_28GBaud_DP-16QAM_1Saperb_18dB_2dBm_0.01-0.6\';
@@ -17,10 +17,10 @@ for i = 1:length(Rolloff)
 end
 
 %% Structure
-for i = 1:length(Rolloff)
-    Path = 'F:\tempo sim data\224Gbpers_28GBaud_DP-16QAM_1Saperb_18dB_2dBm_0.01-0.6\';
-    File = dir(fullfile(Path,strcat('224Gbpers_28GBaud_DP-16QAM_1Saperb_18dB_2dBm_',num2str(Rolloff(i)),'*')));
-    filenames = {File.name};  %get corresponding 400 files of the rolloff in current loop
+for i = 1:length(OSNR)
+    Path = 'F:\tempo sim data\112Gbpers_28GBaud_DP-QPSK_1Saperb_2dBm_0.01\';
+    File = dir(fullfile(Path,strcat('112Gbpers_28GBaud_DP-16QAM_1Saperb_',num2str(OSNR),'dB_2dBm_*')));
+    filenames = {File.name};  %get corresponding 200 files of the OSNR in current loop
     for ii = 1:length(filenames)
         load(strcat(Path,filenames{ii}))
         
@@ -31,7 +31,7 @@ for i = 1:length(Rolloff)
         Params.SymbolRate = SymbolRate;
         Params.OSNR = OSNR;
         Params.BitRate = SymbolRate*8;
-        
+        filename_no = strcat('112Gbpers_28GBaud_DP-16QAM_1Saperb_',num2str(OSNR),'dB_2dBm_131072_400km_No.',num2str(ii),'.mat')
         save(filenames{ii},'InputPort1','InputPort2','InputPort3','InputPort4', ...
                            'Params','-v7.3')
     end
