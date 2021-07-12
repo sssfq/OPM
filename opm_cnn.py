ac150a81dc220277a96bdaa1795234d2a220996f
shenf <404503167@qq.com>
2021-07-12 09:47:55 +0800

    Repetition of doi:10.1364/JOCN.11.000A52 with batchnormalization and dropout

--- opm_cnn.py
+++ opm_cnn.py
@@ -57,33 +57,49 @@ class opm_cnnModel(tf.keras.Model):
         self.layer2 = tf.keras.layers.Dense(units=2048,activation=tf.nn.relu)
         self.layer3 = tf.keras.layers.Dense(units=1)
 
-    def call (self,inputs):
+        self.dropout1 = tf.keras.layers.Dropout(rate=0.5)
+        self.dropout2 = tf.keras.layers.Dropout(rate=0.5)
+
+        self.batchnorm1 = tf.keras.layers.BatchNormalization()
+        self.batchnorm2 = tf.keras.layers.BatchNormalization()
+        self.batchnorm3 = tf.keras.layers.BatchNormalization()
+        self.batchnorm4 = tf.keras.layers.BatchNormalization()
+
+
+    def call (self,inputs,training=False):
         # print(inputs)
         x = self.conv1_1(inputs)
         x = self.conv1_2(x)
+        x = self.batchnorm1(x)
         x = self.pool1(x)
         x = self.conv2(x)
+        x = self.batchnorm2(x)
         x = self.pool2(x)
 
         x = self.flatten(x)
         x = self.layer1(x)
+        x = self.batchnorm3(x,training=training)
+        x = self.dropout1(x,training=training)
         x = self.layer2(x)
+        x = self.batchnorm4(x,training=training)
+        x = self.dropout2(x,training=training)
         x = self.layer3(x)
         output = x
         # output = tf.nn.softmax(x)  #softmax function should be replaced
         return output
 
 # hyperparameter
-num_epochs =5
-batch_size = 8
+num_epochs = 10
+batch_size = 20
 learning_rate = 0.001
 # Instantiate
 signal_size = 512
-label = 'Rolloff'
-train_path = 'F:/tempo sim data/224Gbpers_28GBaud_DP-16QAM_1Saperb_18dB_2dBm_0.01-0.6/train_data/'
-test_path = 'F:/tempo sim data/224Gbpers_28GBaud_DP-16QAM_1Saperb_18dB_2dBm_0.01-0.6/test_data/'
+nominalsymbolrate = 0.5
+label = 'OSNR'
+train_path = 'F:/tempo sim data/112Gbpers_28GBaud_DP-QPSK_1Saperb_2dBm_0.01/train_data/'
+test_path = 'F:/tempo sim data/112Gbpers_28GBaud_DP-QPSK_1Saperb_2dBm_0.01/test_data/'
 
-dataloader = DataLoader(signal_size,label,train_path,test_path)
+dataloader = DataLoader(signal_size,nominalsymbolrate,label,train_path,test_path)
 
 model = opm_cnnModel()
 
@@ -97,11 +113,11 @@ summary_writer = tf.summary.create_file_writer('./tensorboard')
 # training
 num_batches = int(dataloader.num_train_data // batch_size * num_epochs)
 for batch_index in range(num_batches):
-    signal,rolloff = dataloader.get_batch(batch_size)
+    signal,osnr = dataloader.get_batch(batch_size)
     signal = np.rollaxis(signal,1,3)
     with tf.GradientTape() as tape:
-        y_pred = model(signal)
-        loss = tf.keras.losses.mean_squared_error(rolloff,y_pred)   
+        y_pred = model(signal,training=True)
+        loss = tf.keras.losses.mean_squared_error(osnr,y_pred)   
         loss = tf.reduce_mean(loss)
         # print("signal\n:",signal.shape)
         # print("y_pred\n:",y_pred)
@@ -110,8 +126,8 @@ for batch_index in range(num_batches):
         print("batch %d: loss %f" % (batch_index,loss.numpy()))
         with summary_writer.as_default():
             tf.summary.scalar("loss",loss,step=batch_index)
-    grads = tape.gradient(loss,model.variables)
-    optimizer.apply_gradients(grads_and_vars=zip(grads,model.variables))
+    grads = tape.gradient(loss,model.trainable_variables)
+    optimizer.apply_gradients(grads_and_vars=zip(grads,model.trainable_variables))
     
         
 checkpoint.save('./checkpoint/model.ckpt')
@@ -124,7 +140,7 @@ for batch_index in range(num_batches):
     start_index, end_index = batch_index*batch_size, (batch_index + 1) * batch_size
     y_pred = model.predict(np.rollaxis(dataloader.test_data[start_index: end_index],1,3))
     mae.update_state(y_true=dataloader.test_label[start_index: end_index], y_pred=y_pred)
-print("test accuracy: %f" % mae.result())
+    print("test accuracy: %f" % mae.result())
 
 # # restore
 # model_to_be_restored = opm_cnnModel()
